06642720a803cc48c91e13a0ce55720e2c25e255
Carter Chavez <Carter.chavez17@gmail.com>
2018-01-08 15:52:12 -0800

    fix style

--- ticTacToeBrain.js
+++ ticTacToeBrain.js
@@ -14,122 +14,118 @@ class Board {
   this.countOfTurn = 1;
 }
 
-showCurrentBoard() {
-console.log(this.rows)
-}
+  showCurrentBoard() {
+    console.log(this.rows)
+  }
 
-checkBoardForXWin() {
-//horizontal row win
-if ( this.rows[1][0] === ' X' && this.rows[1][2] === ' X' && this.rows[1][4] === ' X') {
-console.log('X wins!')
-}
+  checkBoardForXWin() {
+    //horizontal row win
+    if ( this.rows[1][0] === ' X' && this.rows[1][2] === ' X' && this.rows[1][4] === ' X') {
+      console.log('X wins!')
+    }
 
-if ( this.rows[3][0] === ' X' && this.rows[3][2] === ' X' && this.rows[3][4] === ' X') {
-console.log('X wins!')
-}
+    if ( this.rows[3][0] === ' X' && this.rows[3][2] === ' X' && this.rows[3][4] === ' X') {
+      console.log('X wins!')
+    }
 
-if ( this.rows[5][0] === ' X' && this.rows[5][2] === ' X' && this.rows[5][4] === ' X') {
-console.log('X wins!')
-}
+    if ( this.rows[5][0] === ' X' && this.rows[5][2] === ' X' && this.rows[5][4] === ' X') {
+      console.log('X wins!')
+    }
 
-//Vertical column win
-if ( this.rows[1][0] === ' X' && this.rows[3][0] === ' X' && this.rows[5][0] === ' X') {
-console.log('X wins!')
-}
+    //Vertical column win
+    if ( this.rows[1][0] === ' X' && this.rows[3][0] === ' X' && this.rows[5][0] === ' X') {
+      console.log('X wins!')
+    }
 
-if ( this.rows[1][2] === ' X' && this.rows[3][2] === ' X' && this.rows[5][2] === ' X') {
-console.log('X wins!')
-}
+    if ( this.rows[1][2] === ' X' && this.rows[3][2] === ' X' && this.rows[5][2] === ' X') {
+      console.log('X wins!')
+    }
 
-if ( this.rows[1][4] === ' X' && this.rows[3][4] === ' X' && this.rows[5][4] === ' X') {
-console.log('X wins!')
-}
+    if ( this.rows[1][4] === ' X' && this.rows[3][4] === ' X' && this.rows[5][4] === ' X') {
+      console.log('X wins!')
+    }
 
-//Diagnol win
-if ( this.rows[1][0] === ' X' && this.rows[3][2] === ' X' && this.rows[5][4] === ' X') {
-console.log('X wins!')
-}
+    //Diagnol win
+    if ( this.rows[1][0] === ' X' && this.rows[3][2] === ' X' && this.rows[5][4] === ' X') {
+      console.log('X wins!')
+    }
 
-if ( this.rows[1][4] === ' X' && this.rows[3][2] === ' X' && this.rows[5][0] === ' X') {
-console.log('X wins!')
-}
+    if ( this.rows[1][4] === ' X' && this.rows[3][2] === ' X' && this.rows[5][0] === ' X') {
+      console.log('X wins!')
+    }
+  }
 
-}
+  checkBoardForOWin() {
+    //horizontal row win
+    if ( this.rows[1][0] === ' O' && this.rows[1][2] === ' O' && this.rows[1][4] === ' O') {
+      console.log('O wins!')
+    }
 
-checkBoardForOWin() {
-//horizontal row win
-if ( this.rows[1][0] === ' O' && this.rows[1][2] === ' O' && this.rows[1][4] === ' O') {
-console.log('O wins!')
-}
+    if ( this.rows[3][0] === ' O' && this.rows[3][2] === ' O' && this.rows[3][4] === ' O') {
+      console.log('O wins!')
+    }
 
-if ( this.rows[3][0] === ' O' && this.rows[3][2] === ' O' && this.rows[3][4] === ' O') {
-console.log('O wins!')
-}
+    if ( this.rows[5][0] === ' O' && this.rows[5][2] === ' O' && this.rows[5][4] === ' O') {
+      console.log('O wins!')
+    }
 
-if ( this.rows[5][0] === ' O' && this.rows[5][2] === ' O' && this.rows[5][4] === ' O') {
-console.log('O wins!')
-}
+    //Vertical column win
+    if ( this.rows[1][0] === ' O' && this.rows[3][0] === ' O' && this.rows[5][0] === ' O') {
+      console.log('O wins!')
+    }
 
-//Vertical column win
-if ( this.rows[1][0] === ' O' && this.rows[3][0] === ' O' && this.rows[5][0] === ' O') {
-console.log('O wins!')
-}
+    if ( this.rows[1][2] === ' O' && this.rows[3][2] === ' O' && this.rows[5][2] === ' O') {
+      console.log('O wins!')
+    }
 
-if ( this.rows[1][2] === ' O' && this.rows[3][2] === ' O' && this.rows[5][2] === ' O') {
-console.log('O wins!')
-}
+    if ( this.rows[1][4] === ' O' && this.rows[3][4] === ' O' && this.rows[5][4] === ' O') {
+      console.log('O wins!')
+    }
 
-if ( this.rows[1][4] === ' O' && this.rows[3][4] === ' O' && this.rows[5][4] === ' O') {
-console.log('O wins!')
-}
+    //Diagnol win
+    if ( this.rows[1][0] === ' O' && this.rows[3][2] === ' O' && this.rows[5][4] === ' O') {
+      console.log('O wins!')
+    }
 
-//Diagnol win
-if ( this.rows[1][0] === ' O' && this.rows[3][2] === ' O' && this.rows[5][4] === ' O') {
-console.log('O wins!')
-}
+    if ( this.rows[1][4] === ' O' && this.rows[3][2] === ' O' && this.rows[5][0] === ' O') {
+      console.log('O wins!')
+    }
+  }
 
-if ( this.rows[1][4] === ' O' && this.rows[3][2] === ' O' && this.rows[5][0] === ' O') {
-console.log('O wins!')
-}
-
-}
-
-move(row, column) {
-if ( row === 2 || row === 4 || column === 1 || column === 3 ) {
-console.log(`
-oooooooooo                     oooo          oooo     oooo                                           ooooooooooo                        
- 888    888   ooooooo     ooooo888            8888o   888    ooooooo   oooo   oooo  ooooooooo8       88  888  88 oo oooooo   oooo   oooo
- 888oooo88    ooooo888  888    888            88 888o8 88  888     888  888   888  888oooooo8            888      888    888  888   888 
- 888    888 888    888  888    888            88  888  88  888     888   888 888   888                   888      888          888 888  
-o888ooo888   88ooo88 8o   88ooo888o          o88o  8  o88o   88ooo88       888       88oooo888          o888o    o888o           8888   
-                                                                                                                              o8o888    
-     o                               o88                                                                                                
-    888       oooooooo8   ooooooo    oooo  oo oooooo                                                                                    
-   8  88    888    88o    ooooo888    888   888   888                                                                                   
-  8oooo88    888oo888o  888    888    888   888   888                                                                                   
-o88o  o888o 888     888  88ooo88 8o  o888o o888o o888o                                                                                  
-             888ooo888                                               
-`)
-return
-}
-
-if ( this.rows[row][column] !== '  ') {
-console.log('someone is already there, try again')
-return;
-}
-if ( this.countOfTurn % 2 === 0 ) {
-this.rows[row][column] = ' O'
-this.countOfTurn += 1;
-this.showCurrentBoard()
-this.checkBoardForOWin()
-
-} else {
-this.rows[row][column] = ' X'
-this.countOfTurn += 1;
-this.showCurrentBoard()
-this.checkBoardForXWin()
-}
-}
+  move(row, column) {
+    if ( row === 2 || row === 4 || column === 1 || column === 3 ) {
+      console.log(`
+        oooooooooo                     oooo          oooo     oooo                                           ooooooooooo                        
+         888    888   ooooooo     ooooo888            8888o   888    ooooooo   oooo   oooo  ooooooooo8       88  888  88 oo oooooo   oooo   oooo
+         888oooo88    ooooo888  888    888            88 888o8 88  888     888  888   888  888oooooo8            888      888    888  888   888 
+         888    888 888    888  888    888            88  888  88  888     888   888 888   888                   888      888          888 888  
+        o888ooo888   88ooo88 8o   88ooo888o          o88o  8  o88o   88ooo88       888       88oooo888          o888o    o888o           8888   
+                                                                                                                                      o8o888    
+             o                               o88                                                                                                
+            888       oooooooo8   ooooooo    oooo  oo oooooo                                                                                    
+           8  88    888    88o    ooooo888    888   888   888                                                                                   
+          8oooo88    888oo888o  888    888    888   888   888                                                                                   
+        o88o  o888o 888     888  88ooo88 8o  o888o o888o o888o                                                                                  
+                     888ooo888                                               
+      `)
+      return
+    }
+    if ( this.rows[row][column] !== '  ') {
+      console.log('someone is already there, try again')
+      return;
+    }
+    if ( this.countOfTurn % 2 === 0 ) {
+      this.rows[row][column] = ' O'
+      this.countOfTurn += 1;
+      this.showCurrentBoard()
+      this.checkBoardForOWin()
+    } else {
+      this.rows[row][column] = ' X'
+      this.countOfTurn += 1;
+      this.showCurrentBoard()
+      this.checkBoardForXWin()
+    }
+  }
 }
 
 let x = new Board();
